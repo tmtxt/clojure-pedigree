a1eb646af9d2d14f0b931600ab6fa8f0095d9af9
Truong <me@truongtx.me>
2015-03-31 22:19:35 +0700

    add person model

---
+++ src/app/models/person.clj
@@ -0,0 +1,38 @@
+(ns app.models.person
+    (:use [korma.core]))
+
+;;; Basic Korma model structure
+;;; see more at http://sqlkorma.com/docs
+
+(defentity person
+  ;; Basic configuration
+
+  ;; Table, by default the name of the entity
+  (table :tbl_person)
+
+  ;; Primary key, by default "id"
+  ;; This line is unnecessary, it's used for relationships joins.
+  (pk :id)
+
+  ;; Default fields for selects
+  ;; (entity-fields :column1 :column2)
+
+  ;; Relationships, uncomment or add more as necessary
+
+  ;; assumes users.id = address.users_id
+  ;; (has-one address)
+
+  ;; assumes users.id = email.users_id
+  ;; but gets the results in a second query
+  ;; for each element
+  ;; (has-many email)
+
+  ;; assumes users.account_id = account.id
+  ;; (belongs-to account)
+
+  ;; assumes a table users_posts with columns users_id
+  ;; and posts_id
+  ;; like has-many, also gets the results in a second
+  ;; query for each element
+  ;; (many-to-many posts :users_posts)
+  )
