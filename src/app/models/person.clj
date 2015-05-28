ad0174734a4b07386e0bc83bd39b8d97201bbd51
Truong <me@truongtx.me>
2015-05-28 22:17:39 +0700

    function for creating init root person in system

--- src/app/models/person.clj
+++ src/app/models/person.clj
@@ -1,6 +1,8 @@
 (ns app.models.person
   (:require [korma.core :refer :all]
-            [app.util.dbUtil :as db-util]))
+            [app.util.dbUtil :as db-util]
+            [clojurewerkz.neocons.rest.nodes :as nn]
+            [config.neo4j :refer [conn]]))
 
 (defentity person
   ;; Table, by default the name of the entity
@@ -11,5 +13,7 @@
 (defn create-init-person []
   (when (db-util/table-empty? person)
     (let [root (insert person
-                       (values {:full_name "Root Person"}))]
+                       (values {:full_name "Root Person"}))
+          node (nn/create conn {:user_id (root :id)
+                                :is_root true})]
       root)))
