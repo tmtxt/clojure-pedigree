c68f9d4479723e631add918160dd936bd582671a
Truong <me@truongtx.me>
2015-06-04 23:29:16 +0700

    add authen util module

---
+++ src/app/util/authen.clj
@@ -0,0 +1,31 @@
+(ns app.util.authen
+  (:require [crypto.password.bcrypt :as crypto]
+            [app.models.user :as user-model]
+            [app.util.dbUtil :as db-util]))
+
+(defn authen
+  "Authenticate the input username and password"
+  [username password]
+  (let [user (db-util/find-by-attrs user-model/user {:username username})]
+    (if user
+      (crypto/check password (:password user))
+      false)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
