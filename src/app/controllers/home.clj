7d57ffd222f618f61eb6b70a9d4f5cea20be306e
Truong <me@truongtx.me>
2015-06-04 23:38:20 +0700

    basic routes for login

--- src/app/controllers/home.clj
+++ src/app/controllers/home.clj
@@ -10,36 +10,40 @@
 (defn home []
   (layout/render "home/index.html" {:name (config :site-name)}))
 
+;; (defn login-authenticate [request]
+;;   (let [session (:session request)
+;;         updated-session (assoc session :identity "abc")]
+;;     (println updated-session)
+;;     (-> (redirect "/show") (assoc :session updated-session)))
+;;   )
+
+;; (defn show [request]
+;;   (println "show")
+;;   (println (:session request))
+;;   (println (authenticated? request))
+;;   )
+
+;; (defn testr [request]
+;;   (println "authen")
+;;   (let [session (:session request)
+;;         updated-session (assoc session :identity "abc")]
+;;     (println updated-session)
+;;     (let [temp (-> (redirect "/show") (assoc :session updated-session))]
+;;       (println temp)
+;;       temp)
+;;     )
+;;   )
+
+(defn login-render [request]
+  )
+
 (defn login-authenticate [request]
-  (let [session (:session request)
-        updated-session (assoc session :identity "abc")]
-    (println updated-session)
-    (-> (redirect "/show") (assoc :session updated-session)))
-  )
-
-(defn show [request]
-  (println "show")
-  (println (:session request))
-  (println (authenticated? request))
-  )
-
-(defn testr [request]
-  (println "authen")
-  (let [session (:session request)
-        updated-session (assoc session :identity "abc")]
-    (println updated-session)
-    (let [temp (-> (redirect "/show") (assoc :session updated-session))]
-      (println temp)
-      temp)
-    )
   )
 
 (defroutes home-routes
   (GET "/" [] (home))
-  ;; (GET "/write" [] (session/put! :user "hello"))
-  (GET "/show" [] show)
-  (POST "/login" [] login-authenticate)
-  (GET "/test" [] testr))
+  (GET "/login" [] login-render)
+  (POST "/login" [] login-authenticate))
 
 
 
