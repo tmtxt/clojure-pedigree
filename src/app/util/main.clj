64871fa49e9469e4aae72d014caaa233b55ce174
Truong <me@truongtx.me>
2015-06-05 18:38:02 +0700

    add util function for getting param from request

--- src/app/util/main.clj
+++ src/app/util/main.clj
@@ -1,2 +1,6 @@
 (ns app.util.main
   (:import [org.postgresql.util PGobject]))
+
+(defn param "Get the param from the request"
+  [request name & [default]]
+  (get-in request [:form-params name] default))
