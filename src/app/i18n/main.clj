fcd3d0517c481dd0a4de6d9fd37fa78590b34323
Truong <me@truongtx.me>
2015-06-06 14:46:24 +0700

    update make-t to make t from request, string, keyword

--- src/app/i18n/main.clj
+++ src/app/i18n/main.clj
@@ -11,6 +11,11 @@
 (def t (tower/make-t tconfig))
 
 (defn make-t
-  "Make t based from the input local"
-  [locale]
-  (fn [key] (t locale key)))
+  "Make t from the input local"
+  [locale-or-request]
+  (let [locale (cond
+                 (map? locale-or-request) (get-in locale-or-request [:session :locale] :en)
+                 (string? locale-or-request) (keyword locale-or-request)
+                 (keyword? locale-or-request) locale-or-request
+                 :else :en)]
+    (fn [key] (t locale key))))
