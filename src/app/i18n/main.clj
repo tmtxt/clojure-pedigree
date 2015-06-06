466e10bcc19c04c33792e3dac725eefb0dfc4ba2
Truong <me@truongtx.me>
2015-06-06 14:33:15 +0700

    add function for make-t

--- src/app/i18n/main.clj
+++ src/app/i18n/main.clj
@@ -1,8 +1,16 @@
 (ns app.i18n.main
   (:require [app.i18n.en :as en]
-            [app.i18n.vi :as vi]))
+            [app.i18n.vi :as vi]
+            [taoensso.tower :as tower]))
 
 (def tconfig
   {:dictionary (merge en/dict vi/dict)
    :dev-mode? false
    :fallback-locale :en})
+
+(def t (tower/make-t tconfig))
+
+(defn make-t
+  "Make t based from the input local"
+  [locale]
+  (fn [key] (t locale key)))
