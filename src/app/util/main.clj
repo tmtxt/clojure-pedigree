aae684c38751f836d855acb687f74aa0c34669b2
Truong <me@truongtx.me>
2015-06-07 13:40:37 +0700

    update param function to check for request method

--- src/app/util/main.clj
+++ src/app/util/main.clj
@@ -3,4 +3,7 @@
 
 (defn param "Get the param from the request"
   [request name & [default]]
-  (get-in request [:form-params name] default))
+  (let [method (get request :request-method)]
+    (cond
+      (= method :get) (get-in request [:query-params name] default)
+      (= method :post) (get-in request [:form-params name] default))))
