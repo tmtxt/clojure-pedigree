906c79da88cf2a082d033a85a4b2a7d0f5f18d7b
Truong <me@truongtx.me>
2015-06-27 23:15:48 +0700

    basic function for creating node with merge

--- src/app/util/neo4j/statement.clj
+++ src/app/util/neo4j/statement.clj
@@ -2,6 +2,8 @@
   (:require [clojurewerkz.neocons.rest.nodes :as nn]
             [clojurewerkz.neocons.rest.transaction :as tx]))
 
+(defn get-label [label] (if (keyword? label) (name label) label))
+
 ;;; (create-node :person {:name "hello" :age 18})
 (defn create-node
   "Construct the create node cypher statement.
@@ -9,6 +11,17 @@
   The function returns the statement string.
   Input value is a props map and the label (keyword or string)"
   [label props]
-  (let [_label (if (keyword? label) (name label) label)]
+  (let [_label (get-label label)]
     (tx/statement (format "CREATE (n:`%s` {props}) RETURN n" _label)
+                  {:props props})))
+
+(defn create-merge-node
+  "Create node using Merge"
+  [label props]
+  (let [_label (get-label label)]
+    (tx/statement (format "MERGE (n:%s {%s}) RETURN n" _label
+                          (clojure.string/join
+                           ", "
+                           (map (fn [[key val]] (str (name key) ": " "{props}." (name key))) props))
+                          )
                   {:props props})))
