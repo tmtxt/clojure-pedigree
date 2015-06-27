bb69dbb0cc9e95e3b69a0b2a4a81611158f0e640
Truong <me@truongtx.me>
2015-06-27 22:29:37 +0700

    update function signature

--- src/app/util/neo4j/statement.clj
+++ src/app/util/neo4j/statement.clj
@@ -2,12 +2,13 @@
   (:require [clojurewerkz.neocons.rest.nodes :as nn]
             [clojurewerkz.neocons.rest.transaction :as tx]))
 
+;;; (create-node :person {:name "hello" :age 18})
 (defn create-node
   "Construct the create node cypher statement.
   The statement returns the inserted node.
   The function returns the statement string.
   Input value is a props map and the label (keyword or string)"
-  [props label]
+  [label props]
   (let [_label (if (keyword? label) (name label) label)]
     (tx/statement (format "CREATE (n:`%s` {props}) RETURN n" _label)
                   {:props props})))
