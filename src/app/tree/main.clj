ebe0c42d6c766675cc0884a8b72db6902e4ee517
Truong <me@truongtx.me>
2015-07-05 11:19:26 +0700

    function signature for specifying depth

--- src/app/tree/main.clj
+++ src/app/tree/main.clj
@@ -3,15 +3,21 @@
 
 (def default-depth 5)
 
-(defn- get-tree-from-node [root]
-  (println root))
+(defn- get-tree-from-node [root & [depth]]
+  (println root)
+  (println depth))
 
 (defn get-tree
   "Get tree from user id"
   ([]
    (let [root-node (person/find-root-node)]
-     (get-tree-from-node root-node)))
+     (get-tree-from-node root-node default-depth)))
 
   ([user-id]
    (let [root-node (person/find-node-by-user-id user-id)]
-     (get-tree-from-node root-node))))
+     (get-tree-from-node root-node default-depth)))
+
+  ([user-id & {:keys [depth]
+               :or [depth default-depth]}]
+   (let [root-node (person/find-node-by-user-id user-id)]
+     (get-tree-from-node root-node depth))))
