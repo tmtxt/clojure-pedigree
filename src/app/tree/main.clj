7dfcd07513498a6b2aaf9bfce71a55d5c178be85
Truong <me@truongtx.me>
2015-07-13 23:29:54 +0700

    do something rediculuos

--- src/app/tree/main.clj
+++ src/app/tree/main.clj
@@ -1,6 +1,7 @@
 (ns app.tree.main
   (:require [app.models.person :as person]
-            [app.util.neo4j.command :as ncm]))
+            [app.util.neo4j.command :as ncm]
+            [com.rpl.specter :refer :all]))
 
 (def ^{:private true} default-depth 5)
 
@@ -19,14 +20,13 @@
   (let [sub-path (rest path)
         sub-id (first path)
         children (get user-data :children [])
-        idx (.indexOf children sub-id)
+        ;; idx (.indexOf children (second path))
+        temp (first (select [ALL :user-id (fn [id] (= (second path) id))] children))
+        idx (.indexOf children temp)
         user-data2 (assoc user-data :children children)
         ]
+
     (if sub-id
-      ;; (let [sub (recur2 sub-path user-data2)
-      ;;       user-data2 (assoc-in user-data2 [:children (if (= idx -1) 0 idx)] sub)
-      ;;       user-data3 (assoc user-data2 :user-id sub-id)]
-      ;;   user-data3)
       (let [user-data3 (assoc user-data2 :user-id sub-id)
             sub (recur2 sub-path user-data3)
             user-data4 (assoc-in user-data3 [:children (if (= idx -1) 0 idx)] sub)
@@ -35,32 +35,70 @@
       sub-id)
     ))
 
+(defn recur4 [path user-data]
+  (let [sub-path (rest path)
+        sub-id (first path)
+        children (get user-data :children [])
+        temp (first (select [ALL :user-id (fn [id] (= (second path) id))] children))
+        idx (.indexOf children temp)
+        user-data2 (assoc user-data :children children)
+        ]
 
-;; (defn f []
-;;   (let [path [[1 2]
-;;               [1 2 3]
-;;               [1 2 3 5]
-;;               [1 2 3 6]
-;;               [1 2 4]
-;;               [1 2 4 7]
-;;               [1 2 8]
-;;               [1 2 8 9]
-;;               [1 2 8 9 10]
-;;               [1 2 8 9 11]
-;;               [1 2 8 12]
-;;               [1 2 8 12 13]]
-;;         tree
-;;         (reduce (fn [link user-data] (recur2 link user-data)) {} path)
-;;         ;; (recur2 path {})
-;;         ]
-;;     (clojure.pprint/pprint tree)
-;;     ))
+    ;; (if sub-id
+    ;;   (let [user-data3 (assoc user-data2 :user-id sub-id)
+    ;;         sub (recur2 sub-path user-data3)
+    ;;         ;; user-data4 (assoc-in user-data3 [:children (if (= idx -1) 0 idx)] sub)
+    ;;         user-data4 (if (= idx -1)
+    ;;                      (let [new-children (conj children sub)]
+    ;;                        (assoc user-data3 :children new-children)
+    ;;                        )
+    ;;                      (assoc-in user-data3 [:children idx] sub))
+    ;;         ]
+    ;;     user-data4)
+    ;;   sub-id)
+    user-data
+    ))
+
+(defn recur3 [path user-data]
+  (let [children-path (rest path)
+        sub-id (first path)
+        children (get user-data :children [])
+        user-data2 (assoc user-data :children children)]
+    (if sub-id
+      (let [user-data3 (assoc user-data2 :user-id sub-id)
+            sub (recur3 children-path user-data3)
+            user-data4 (assoc user-data3 :children sub)]
+        user-data4)
+      sub-id)
+    ))
 
 (defn f []
-  (let [path [1 2 3 4 5 6]
-        tree (recur2 path {})]
-    (clojure.pprint/pprint tree)
+  (let [path [[1 2]
+              [1 2 3]
+              [1 2 3 5]
+              [1 2 3 6]
+              [1 2 4]
+              [1 2 4 7]
+              [1 2 8]
+              [1 2 8 9]
+              [1 2 8 9 10]
+              [1 2 8 9 11]
+              [1 2 8 12]
+              [1 2 8 12 13]
+              [1 14]]
+        func (fn [user-data link]
+               (println user-data)
+               (recur4 link user-data))
+        tree (reduce func {} path)
+        ]
+    ;; (clojure.pprint/pprint tree)
     ))
+
+;; (defn f []
+;;   (let [path [1 2 3 4 5 6]
+;;         tree (recur2 path {})]
+;;     (clojure.pprint/pprint tree)
+;;     ))
 
 ;; (defn process [path user-data]
 ;;   (let [sub-path (rest path)
