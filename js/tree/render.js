52a83eb08d814164b09f332c6e75404eef9a08f4
Truong <me@truongtx.me>
2015-08-15 21:57:15 +0700

    move update nodes to another function

--- js/tree/render.js
+++ js/tree/render.js
@@ -1,3 +1,4 @@
+////////////////////////////////////////////////////////////////////////////////
 // Main function for rendering
 function render(page) {
   var config = page.config;
@@ -16,6 +17,7 @@ function render(page) {
 }
 exports.render = render;
 
+////////////////////////////////////////////////////////////////////////////////
 // Toggle children
 function toggleAll(d) {
   if (d.children) {
@@ -33,8 +35,10 @@ function toggle(d) {
   }
 }
 
-var id = 0;
-function update(page, source) {
+////////////////////////////////////////////////////////////////////////////////
+// Functions for processing nodes list
+
+function updateNodes(page, source) {
   var config = page.config;
   var duration = config.getTransitionDuration();
   var linkHeight = config.getLinkHeight();
@@ -42,15 +46,11 @@ function update(page, source) {
   var treeData = page.root;
 
   // Calculate Node list position
-  page.nodesList = treeLayout.nodes(treeData).reverse();
-  page.nodesList.forEach(function(d){
-    d.y = d.depth * linkHeight;
-    d.y += 80;
-  });
+  var nodesList = calculateNodesList(page);
 
   // Bind data to the svg nodes (not actual nodes now)
   var nodeGroups = page.rootGroup.selectAll("g.node")
-      .data(page.nodesList, function(d) { return d.id || (d.id = ++id); });
+      .data(nodesList, function(d) { return d.id || (d.id = ++id); });
 
   // ENTER
   // Now actually create new node group if not exist
@@ -65,7 +65,12 @@ function update(page, source) {
       update(page, d); // Update the tree again
     });
   // Person name
-  nodeEnter.append("svg:text");
+  nodeEnter.append("svg:text")
+    .text(function(d) { return d.info.full_name; })
+    .attr("y", -19)
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle")
+    .style("fill-opacity", 1);
   // Person image
   nodeEnter.append("svg:image")
     .attr("xlink:href", function(d){
@@ -87,16 +92,9 @@ function update(page, source) {
       .attr("transform", function(d) {
 			  return "translate(" + d.x + "," + d.y + ")";
       });
-  // update the node circle and text
   nodeUpdate.select("circle")
     .attr("r", 10)
     .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-  nodeUpdate.select("text")
-    .text(function(d) { return d["user-id"]; })
-    .attr("y", -19)
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .style("fill-opacity", 1);
 
   // EXIT
   // Transition exiting nodes to the parent's new position.
@@ -105,9 +103,42 @@ function update(page, source) {
       .attr("transform", function(d) { return "translate(" + source.x + "," + source.y + ")"; })
       .remove();
 
+  return nodesList;
+}
+
+// Function for calculating nodes list position using d3 api
+function calculateNodesList(page) {
+  var config = page.config;
+  var linkHeight = config.getLinkHeight();
+  var treeData = page.root;
+  var treeLayout = page.treeLayout;
+  var nodesList;
+
+  nodesList = treeLayout.nodes(treeData).reverse();
+  nodesList.forEach(function(d){
+    d.y = d.depth * linkHeight;
+    d.y += 80;
+  });
+
+  return nodesList;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions for processing links list
+
+var id = 0;
+function update(page, source) {
+  var config = page.config;
+  var duration = config.getTransitionDuration();
+  var linkHeight = config.getLinkHeight();
+  var treeLayout = page.treeLayout;
+  var treeData = page.root;
+
+  var nodesList = updateNodes(page, source);
+
   // Update the links
   var links = page.rootGroup.selectAll("path.link")
-      .data(page.treeLayout.links(page.nodesList), function(d) { return d.target.id; });
+      .data(page.treeLayout.links(nodesList), function(d) { return d.target.id; });
   links.enter().insert("svg:path", "g")
     .attr("class", "link")
     .attr("d", function(d) {
@@ -132,7 +163,7 @@ function update(page, source) {
   // Util.updateTreeDiagramHeight(page);
 
   // Stash the old positions for transition.
-  page.nodesList.forEach(function(d) {
+  nodesList.forEach(function(d) {
     d.x0 = d.x;
     d.y0 = d.y;
   });
