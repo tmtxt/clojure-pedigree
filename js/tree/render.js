ae765dc2bbfbdd7fe2c9a56fd3b008d8b02eb16b
Truong <me@truongtx.me>
2015-08-15 22:05:41 +0700

    move function for processing nodes to another module

--- js/tree/render.js
+++ js/tree/render.js
@@ -1,5 +1,6 @@
 // Modules
 var Util = require('./util.js');
+var Nodes = require('./nodes.js');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main function for rendering
@@ -23,91 +24,6 @@ exports.render = render;
 ////////////////////////////////////////////////////////////////////////////////
 // Functions for processing nodes list
 
-function updateNodes(page, source) {
-  var config = page.config;
-  var duration = config.getTransitionDuration();
-  var linkHeight = config.getLinkHeight();
-  var treeLayout = page.treeLayout;
-  var treeData = page.root;
-
-  // Calculate Node list position
-  var nodesList = calculateNodesList(page);
-
-  // Bind data to the svg nodes (not actual nodes now)
-  var nodeGroups = page.rootGroup.selectAll("g.node")
-      .data(nodesList, function(d) { return d.id || (d.id = ++id); });
-
-  // ENTER
-  // Now actually create new node group if not exist
-  var nodeEnter = nodeGroups.enter().append("svg:g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + source.x0 + "," + source.y0 + ")"; });
-  // Create the elements inside that node group
-  // The circle to click for expanding
-  nodeEnter.append("svg:circle")
-		.on("click", function(d) {
-      Util.toggle(d);
-      update(page, d); // Update the tree again
-    });
-  // Person name
-  nodeEnter.append("svg:text")
-    .text(function(d) { return d.info.full_name; })
-    .attr("y", -19)
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .style("fill-opacity", 1);
-  // Person image
-  nodeEnter.append("svg:image")
-    .attr("xlink:href", function(d){
-      return d.info.picture;
-    })
-    .attr("x", -20)
-    .attr("y", -68)
-    .attr("height", "40px")
-    .attr("width", "40px")
-    .on('click', function(d){
-      // Util.showInfoModal(d.id);
-    });
-  // NodeMarriage.appendMarriage(page, nodeEnter);
-
-	// UPDATE
-  // Update the data and transition nodes to their new position.
-  var nodeUpdate = nodeGroups.transition()
-      .duration(duration)
-      .attr("transform", function(d) {
-			  return "translate(" + d.x + "," + d.y + ")";
-      });
-  nodeUpdate.select("circle")
-    .attr("r", 10)
-    .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-  // EXIT
-  // Transition exiting nodes to the parent's new position.
-  var nodeExit = nodeGroups.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + source.x + "," + source.y + ")"; })
-      .remove();
-
-  return nodesList;
-}
-
-// Function for calculating nodes list position using d3 api
-function calculateNodesList(page) {
-  var config = page.config;
-  var linkHeight = config.getLinkHeight();
-  var treeData = page.root;
-  var treeLayout = page.treeLayout;
-  var nodesList;
-
-  nodesList = treeLayout.nodes(treeData).reverse();
-  nodesList.forEach(function(d){
-    d.y = d.depth * linkHeight;
-    d.y += 80;
-  });
-
-  return nodesList;
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 // Functions for processing links list
 
@@ -119,7 +35,7 @@ function update(page, source) {
   var treeLayout = page.treeLayout;
   var treeData = page.root;
 
-  var nodesList = updateNodes(page, source);
+  var nodesList = Nodes.updateNodes(page, source);
 
   // Update the links
   var links = page.rootGroup.selectAll("path.link")
@@ -153,3 +69,4 @@ function update(page, source) {
     d.y0 = d.y;
   });
 }
+exports.update = update;
