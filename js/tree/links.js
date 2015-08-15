4d44e20325545b4a79b8fdb4368a61126fc22da4
Truong <me@truongtx.me>
2015-08-15 22:28:46 +0700

    separate function inside links module

--- js/tree/links.js
+++ js/tree/links.js
@@ -2,10 +2,23 @@ function updateLinks(page, source, nodesList) {
   var config = page.config;
   var duration = config.getTransitionDuration();
 
-  // Update the links
-  var links = page.rootGroup.selectAll("path.link")
+  // Bind data to the links (not actually create the elements)
+  var linksGroup = page.rootGroup.selectAll("path.link")
       .data(page.treeLayout.links(nodesList), function(d) { return d.target.id; });
-  links.enter().insert("svg:path", "g")
+
+  enter(page, source, linksGroup);
+  update(page, source, linksGroup);
+  exit(page, source, linksGroup);
+
+}
+exports.updateLinks = updateLinks;
+
+function enter(page, source, linksGroup) {
+  var config = page.config;
+  var duration = config.getTransitionDuration();
+
+  // create missing links if necessary
+  linksGroup.enter().insert("svg:path", "g")
     .attr("class", "link")
     .attr("d", function(d) {
       var o = {x: source.x0, y: source.y0};
@@ -14,10 +27,24 @@ function updateLinks(page, source, nodesList) {
     .transition()
     .duration(duration)
     .attr("d", page.diagonal);
-  links.transition()
+}
+
+function update(page, source, linksGroup) {
+  var config = page.config;
+  var duration = config.getTransitionDuration();
+
+  // transition links
+  linksGroup.transition()
     .duration(duration)
     .attr("d", page.diagonal);
-  links.exit().transition()
+}
+
+function exit(page, source, linksGroup) {
+  var config = page.config;
+  var duration = config.getTransitionDuration();
+
+  // remove un-used links
+  linksGroup.exit().transition()
     .duration(duration)
     .attr("d", function(d) {
       var o = {x: source.x, y: source.y};
@@ -25,4 +52,3 @@ function updateLinks(page, source, nodesList) {
     })
     .remove();
 }
-exports.updateLinks = updateLinks;
