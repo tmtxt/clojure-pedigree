4f1266f380a1f1d289232a3a52523d7e145d7b18
Truong <me@truongtx.me>
2015-08-16 12:24:38 +0700

    function for enable marrage

--- js/tree/marriage.js
+++ js/tree/marriage.js
@@ -11,12 +11,45 @@ function init(page) {
   marriageCheckbox.change(function(){
     if(marriageCheckbox.is(':checked')) {
       console.log('checked');
+      enableMarriage(page);
     } else {
       console.log('no checked');
     }
   });
 }
 exports.init = init;
+
+function enableMarriage(page) {
+  var config = page.config;
+  var duration = config.getTransitionDuration();
+  config.setEnableMarriage(true);
+
+  // get all visible nodes
+  var nodes = d3.selectAll('g.node')[0];
+
+  // loop
+  nodes.forEach(function(node) {
+    var order = 0;
+    _.each(node.__data__.marriage, function(marriage) {
+      // append marriage info to the node group
+      d3.select(node).append("svg:image")
+        .attr("xlink:href", '/assets/img/r.jpg')
+        .attr("class", "marriage-image")
+        .attr("x", -20)
+        .attr("y", -68)
+        .attr("height", "40px")
+        .attr("width", "40px")
+        .datum(marriage)
+        .on('click', function(d){
+          // Util.showInfoModal(d.id);
+        })
+        .transition()
+        .duration(duration)
+        .attr('transform', 'translate (' + ((45 * order) + 45) + ',0)');
+      order = order + 1;
+    });
+  });
+}
 
 function appendMarriages(page, nodeEnter) {
   var config = page.config;
