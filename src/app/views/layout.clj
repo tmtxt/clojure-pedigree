c24547a0f81b918c6bff1c51552f6c25424e6dcc
Truong <me@truongtx.me>
2015-08-16 18:06:06 +0700

    add js version

--- src/app/views/layout.clj
+++ src/app/views/layout.clj
@@ -3,7 +3,8 @@
             [ring.util.response :refer [content-type response]]
             [compojure.response :refer [Renderable]]
             [app.i18n.main :refer [make-layout-tran]]
-            [app.models.user :refer [get-user-from-request]]))
+            [app.models.user :refer [get-user-from-request]]
+            [app.views.version :refer [version]]))
 
 (parser/set-resource-path! (clojure.java.io/resource "templates"))
 (parser/cache-off!)
@@ -15,5 +16,6 @@
   (let [args (if params params {})
         template-params {:layout (make-layout-tran request)
                          :page params
-                         :user (get-user-from-request request)}]
+                         :user (get-user-from-request request)
+                         :version (str "?version=" version)}]
     (utf-8-response (parser/render-file template template-params))))
