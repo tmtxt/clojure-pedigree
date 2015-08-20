73ac479de6886aa1fbc3191df61e7259f2def9ef
Truong <me@truongtx.me>
2015-08-20 21:49:14 +0700

    basic function for adding child

--- src/app/controllers/person.clj
+++ src/app/controllers/person.clj
@@ -6,15 +6,22 @@
             [app.util.main :as util]
             [app.util.person :as person-util]
             [app.models.person :as person]
+            [app.util.neo4j.command :as ncm]
+            [config.neo4j :refer [conn]]
+            [app.models.pedigreeRelation :as prl]
+            [korma.db :as kd]
             [app.i18n.main :refer [make-t-with-scope make-page-tran]]
             [korma.core :as kc]))
 
+(defn- find-person-from-request [request param-name]
+  (-> request
+      (util/param param-name)
+      (util/parse-int)
+      (person/find-by-person-id)))
+
 (defn add-child [request]
   ;; check parent exists
-  (let [parent (-> request
-                   (util/param "parentId")
-                   (util/parse-int)
-                   (person/find-by-person-id))]
+  (let [parent (find-person-from-request request "parentId")]
     (if parent
       ;; get all the marriage partner of this parent
       (let [partners (person/find-partners (:id parent))
@@ -33,6 +40,29 @@
       ;; render error page
       (error/render "parent not found"))))
 
+(defn add-child-process [request]
+  ;; check parent exists
+  (let [parent (find-parent-from-request request "parent_id")
+        partner (find-parent-from-request request "partner_id")
+        child-name (util/param request "child_name")
+        child-order (-> request
+                        (util/param "child_order" 0)
+                        (util/parse-int 0))]
+    (cond
+      (not parent) "parent not found"
+      (not partner) "partner not found"
+      :else
+      (ncm/with-transaction conn
+        (kd/transaction
+         (let [{father :father mother :mother} (person-util/determine-father-mother parent partner)
+               child-node (-> {:full_name child-name}
+                              (person/add-person)
+                              (:node))
+               father-node (person/find-node-by-user-id (:id father))
+               mother-node (person/find-node-by-user-id (:id mother))]
+           (prl/add-child father-node mother-node child-node child-order)
+           ))))))
+
 (defn add-parent [request]
   "hello")
 
@@ -42,5 +72,6 @@
 (def person-routes
   (context "/person" []
            (GET "/addChild/parentId/:parentId" [] add-child)
+           (POST "/addChild" [] add-child-process)
            (GET "/addParent/childId/:childId" [] add-parent)
            (GET "/addPartner/partnerId/:partnerId" [] add-partner)))
--- src/app/util/person.clj
+++ src/app/util/person.clj
@@ -15,3 +15,16 @@
       (= person-gender (:les gender-map)) [:father-fullname :mother-fullname]
       :else [:father-fullname :mother-fullname]
       )))
+
+(defn determine-father-mother
+  "Determine the input parent and partner to see who is the father, who is the mother"
+  [parent partner]
+  (let [parent-gender (:gender parent)
+        gender-map person/GENDERS_MAP]
+    (cond
+      (= parent-gender (:male gender-map)) {:father parent :mother partner}
+      (= parent-gender (:female gender-map)) {:father partner :mother parent}
+      (= parent-gender (:gay gender-map)) {:father partner :mother parent}
+      (= parent-gender (:les gender-map)) {:father parent :mother partner}
+      :else {:father parent :mother partner}
+      )))
