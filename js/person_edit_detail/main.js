4dd65b0745c6cd3afa6ed197cfe9780f798d4a83
Truong <me@truongtx.me>
2015-08-30 23:02:21 +0700

    move profile view to react

--- js/person_edit_detail/main.js
+++ js/person_edit_detail/main.js
@@ -1,5 +1,6 @@
 // Libs
 var jquery = require('jquery');
+var React = require('react');
 
 // Components
 var birthDateInput = jquery('.js-birthdate-input');
@@ -12,3 +13,10 @@ birthDateInput.datepicker({
 deathDateInput.datepicker({
   language: 'vi'
 });
+
+//
+var ProfileView = require('./profile.jsx');
+React.render(
+  React.createElement(ProfileView),
+  document.getElementById('js-profile-container')
+);
---
+++ js/person_edit_detail/profile.jsx
@@ -0,0 +1,81 @@
+var React = require('react');
+
+var ProfileView = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="profile-header">
+          Hồ sơ
+        </div>
+
+        <div className="profile-body">
+          <div className="profile-body-row">
+            <div className="profile-body-left">
+              Tên
+            </div>
+            <div className="profile-body-right">
+              <input className="form-control" name="" type="text" value=""/>
+            </div>
+          </div>
+
+          <div className="profile-body-row">
+            <div className="profile-body-left">
+              Ngày sinh
+            </div>
+            <div className="profile-body-right">
+              <input className="form-control js-birthdate-input" name="" type="text" value=""/>
+            </div>
+          </div>
+
+          <div className="profile-body-row">
+            <div className="profile-body-left">
+              Tình trạng
+            </div>
+            <div className="profile-body-right">
+              <select className="form-control">
+                <option value="">1</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="profile-body-row">
+            <div className="profile-body-left">
+              Ngày mất
+            </div>
+            <div className="profile-body-right">
+              <input className="form-control js-deathdate-input" name="" type="text" value=""/>
+            </div>
+          </div>
+
+          <div className="profile-body-row">
+            <div className="profile-body-left">
+              Giới tính
+            </div>
+            <div className="profile-body-right">
+              <input className="form-control" name="" type="text" value=""/>
+            </div>
+          </div>
+
+          <div className="profile-body-row">
+            <div className="profile-body-left">
+              Điện thoại
+            </div>
+            <div className="profile-body-right">
+              <input className="form-control" name="" type="text" value=""/>
+            </div>
+          </div>
+
+          <div className="profile-body-row">
+            <div className="profile-body-left">
+              Địa chỉ
+            </div>
+            <div className="profile-body-right">
+              <textarea className="form-control" cols="30" id="" name="" rows="3"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+module.exports = ProfileView;
