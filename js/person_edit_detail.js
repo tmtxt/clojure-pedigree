57076df107d948e59867c7963cfaf9bb8f24a5b4
Truong <me@truongtx.me>
2015-08-30 22:18:11 +0700

    locale for date picker

--- js/person_edit_detail.js
+++ js/person_edit_detail.js
@@ -3,6 +3,12 @@ var jquery = require('jquery');
 
 // Components
 var birthDateInput = jquery('.js-birthdate-input');
+var deathDateInput = jquery('.js-deathdate-input');
 
 // Init
-birthDateInput.datepicker();
+birthDateInput.datepicker({
+  language: 'vi'
+});
+deathDateInput.datepicker({
+  language: 'vi'
+});
