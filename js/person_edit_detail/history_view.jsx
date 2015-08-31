44f449bc98a40de0c125f9023374fabe583dbe74
Truong <me@truongtx.me>
2015-08-31 22:43:25 +0700

    init markdown editor for history

--- js/person_edit_detail/history_view.jsx
+++ js/person_edit_detail/history_view.jsx
@@ -1,6 +1,14 @@
 var React = require("react");
+var jquery = require("jquery");
 
 var HistoryView = React.createClass({
+  componentDidMount: function() {
+    var historyEditor = jquery(".js-history-editor");
+    historyEditor.markdown({
+      iconlibrary: "fa"
+    });
+  },
+
   render: function() {
     return (
       <div className="history-container">
@@ -8,7 +16,7 @@ var HistoryView = React.createClass({
           History
         </div>
         <div className="history-body">
-          <textarea className="form-control" cols="30" id="" name="" rows="10"></textarea>
+          <textarea className="form-control js-history-editor" cols="30" id="" name="" rows="10"></textarea>
         </div>
       </div>
     );
