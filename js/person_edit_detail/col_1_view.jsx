318e4afe78c8d4a7515962749477c1485e5831d5
Truong <me@truongtx.me>
2015-08-31 21:07:38 +0700

    trigger file select when click on select image

--- js/person_edit_detail/col_1_view.jsx
+++ js/person_edit_detail/col_1_view.jsx
@@ -1,14 +1,29 @@
 var React = require("react");
+var jquery = require("jquery");
+var defaultLink = "/assets/img/userbasic.jpg";
 
 var Col1View = React.createClass({
+  getInitialState: function() {
+    return {imageLink: defaultLink};
+  },
+
+  handleSelectImage: function(e) {
+    e.preventDefault();
+    var pictureInput = jquery(".js-picture-input");
+    pictureInput.trigger("click");
+  },
+
   render: function() {
     return (
       <div className="editperson-col-1">
         <div className="col-1-img">
-          <img className="img-responsive img-thumbnail" alt="" src="/assets/img/t.jpg"/>
+          <img className="img-responsive img-thumbnail" alt="" src={this.state.imageLink}/>
+          <input ref="pictureInput" name="picture"
+                 type="file" accept="image/*" className="hidden js-picture-input"/>
         </div>
         <div className="col-1-buttons">
-          <button className="btn btn-success">Select</button>
+          <button onClick={this.handleSelectImage}
+                  className="btn btn-success">Select</button>
           <button className="btn btn-danger">Delete</button>
         </div>
       </div>
