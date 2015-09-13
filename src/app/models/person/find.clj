0be3e09532e726e82cbcd6318d1dd7576f814df9
Truong <me@truongtx.me>
2015-09-13 17:44:45 +0700

    basic for include node

--- src/app/models/person/find.clj
+++ src/app/models/person/find.clj
@@ -1,6 +1,8 @@
 (ns app.models.person.find
   (:require [korma.core :as kc]
             [app.util.db-util :as db-util]
+            [app.neo4j.node :as node]
+            [app.neo4j.main :as neo4j]
             [app.models.person.definition :refer [person]]
             [app.models.person.util :as model-util]
             [app.models.person.prepare :as prepare]
@@ -41,19 +43,36 @@
                                         (assoc criteria :alive-status))
                       criteria)
          criteria (if birth-date (->> birth-date
-                                        (process-birth-date)
-                                        (assoc criteria :birth-date))
+                                      (process-birth-date)
+                                      (assoc criteria :birth-date))
                       criteria)
          criteria (if death-date (->> death-date
-                                        (process-death-date)
-                                        (assoc criteria :death-date))
+                                      (process-death-date)
+                                      (assoc criteria :death-date))
                       criteria)]
     (model-util/camel-keys->snake-keys criteria)))
 
-(defn pg-find-person-by
-  [criteria]
+(defn find-entity [criteria]
   (->> criteria
        (process-criteria)
        (kc/where)
        (kc/select person)
        (first)))
+
+(defn find-node-from-entity [entity]
+  (if entity
+    (node/find-by-props :person {:person_id (:id entity)})
+    nil))
+
+;;; you should use this function
+(defn find-person-by
+  [criteria & {:keys [include-node include-marriage]
+               :or {include-node false
+                    include-marrage false}}]
+  (let [result {}
+        person-entity (find-entity criteria)
+        person-node (if include-node (find-node-from-entity person-entity) nil)]
+    (assoc result
+           :entity person-entity
+           :node person-node)
+    ))
