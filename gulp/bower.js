6429062137672caa2505d4b6201f90adecc46386
Tran Xuan Truong <truong.tran@lazada.com>
2015-11-06 14:16:56 +0700

    separate gulp symlink

--- gulp/bower.js
+++ gulp/bower.js
@@ -1,9 +1,27 @@
 var gulp = require('gulp');
 var bower = require('bower');
+var path = require('path');
+var fs = require('fs');
 
 gulp.task('bower', function(cb){
   bower.commands.install([], {save: true}, {})
     .on('end', function(installed){
       cb();
     });
+});
+
+gulp.task('symlink', ['bower'], function(){
+  var source = path.normalize(process.cwd() + '/./resources/public/bower');
+  var dest = path.normalize('./sass/bower');
+
+  // check if the destination is already exist
+  var stat;
+  try{
+    stat = fs.lstatSync(dest);
+    // exist, remove it
+    fs.unlinkSync(dest);
+  } catch(e){
+    // Not exist, just don't care
+  }
+  fs.symlinkSync(source, dest);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -59,22 +59,6 @@ gulp.task('sass-watch', function(){
   gulp.watch('./sass/**/*.scss', ['sass-dev-watch']);
 });
 
-gulp.task('symlink', ['bower'], function(){
-  var source = path.normalize(process.cwd() + '/./resources/public/bower');
-  var dest = path.normalize('./sass/bower');
-
-  // check if the destination is already exist
-  var stat;
-  try{
-    stat = fs.lstatSync(dest);
-    // exist, remove it
-    fs.unlinkSync(dest);
-  } catch(e){
-    // Not exist, just don't care
-  }
-  fs.symlinkSync(source, dest);
-});
-
 //////////////////////////////////////////////////////////////////////
 // update version
 gulp.task('update-version', function(){
