5f7ebda03e2514edd3fe63a528847254d2032471
Truong <me@truongtx.me>
2015-11-07 15:51:17 +0700

    generate random file name for uploaded file

--- src/app/controllers/person/util.clj
+++ src/app/controllers/person/util.clj
@@ -1,6 +1,11 @@
 (ns app.controllers.person.util
   (:require [app.util.main :as util]
-            [app.models.person :as person]))
+            [app.models.person :as person]
+            [clojure.java.io :as io]
+            [config.main :refer [config]]
+            [clj-time.core :as time]
+            [clj-time.format :as time-format]
+            [digest :refer [sha-256]]))
 
 (defn find-person-from-request [request param-name]
   (let [param-name (keyword param-name)
@@ -25,7 +30,20 @@
    :gender gender
    :phone_no phone})
 
+(defn generate-random-name []
+  (let [now (time/now)
+        time-formatter (time-format/formatters :basic-date-time)
+        time-string (time-format/unparse time-formatter now)]
+    (sha-256 time-string)))
+
+(defn store-person-picture [params]
+  (let [{{temp-file :tempfile} :picture} params
+        file-name (generate-random-name)
+        file-name (str file-name ".jpg")]
+    (io/copy temp-file (io/file "resources" "public" file-name))))
+
 (defn create-person-from-request [request]
   (let [params (util/params request)
+        _ (store-person-picture params)
         person-data (params-to-person-data params)]
     (person/add-person person-data)))
