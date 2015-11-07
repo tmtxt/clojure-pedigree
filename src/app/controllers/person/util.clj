6c34a6099a96caf22c1e3b5269c200e9910fa68e
Truong <me@truongtx.me>
2015-11-07 15:07:15 +0700

    move common functions to  controller util

--- src/app/controllers/person/util.clj
+++ src/app/controllers/person/util.clj
@@ -7,3 +7,25 @@
         params (util/params request)
         param (get params param-name)]
     (-> param util/parse-int person/find-person-by-id :entity)))
+
+(defn params-to-person-data
+  [{full-name :name
+    birth-date :birthdate
+    status :status
+    gender :gender
+    death-date :deathdate
+    phone :phone
+    address :address}]
+
+  {:full_name full-name
+   ;; :birth_date birth-date
+   ;; :death_date death-date
+   :alive_status status
+   :address address
+   :gender gender
+   :phone_no phone})
+
+(defn create-person-from-request [request]
+  (let [params (util/params request)
+        person-data (params-to-person-data params)]
+    (person/add-person person-data)))
