06dd0df03d37b4c29b4e393b70892fa62fc8be48
Truong <me@truongtx.me>
2015-11-08 20:01:46 +0700

    implement function for adding from child

--- src/app/controllers/person/add/from_child.clj
+++ src/app/controllers/person/add/from_child.clj
@@ -4,7 +4,8 @@
             [app.util.person :as person-util]
             [app.models.person :as person]
             [korma.db :as kd]
-            [app.controllers.person.util :as controller-util]))
+            [app.controllers.person.util :as controller-util]
+            [app.models.pedigree-relation :as prl]))
 
 (defn process-get-request [request]
   (neo4j/with-transaction
@@ -19,5 +20,13 @@
 (defn process-post-request [request]
   (neo4j/with-transaction
     (kd/transaction
-
-     )))
+     (if-let [child (controller-util/find-person-from-request request :childId)]
+       (let [person-result (controller-util/create-person-from-request request)
+             person-entity (person-result :entity)
+             person-node (person-result :node)
+             child-node (person/find-node-by-person-id (:id child))
+             parent-role (person-util/determine-father-mother-single person-entity)]
+         (if (= parent-role :father)
+           (prl/add-child-for-father person-node child-node)
+           (prl/add-child-for-mother person-node child-node)))
+       "child nil"))))
