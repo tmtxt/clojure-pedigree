39b92e119022bafbb8e74dfb46a93f8f06529d0a
Truong <me@truongtx.me>
2015-11-28 22:35:19 +0700

    script for backing up postgres data

--- ansible/templates/backup.sh
+++ ansible/templates/backup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Create temp folder
+TEMP_DIR="{{ project_dir }}/temp"
+mkdir $TEMP_DIR
+
+# Backup folders
 CURRENT_TIME="$(date +%Y-%m-%d-%H-%M-%S)"
 FILE_EXT="zip"
 FILE_NAME="{{ project_dir }}/backup/backup-on-$CURRENT_TIME.$FILE_EXT"
@@ -16,3 +21,18 @@ do
     cd $path/..
     zip -r -u $FILE_NAME $dir
 done
+
+# Backup postgres database
+PGDATABASE={{ db_name }}
+PGHOST={{ db_host }}
+PGUSER={{ db_user }}
+PGPASSWORD={{ db_password }}
+PGPORT=5432
+
+mkdir $TEMP_DIR/postgres
+cd $TEMP_DIR/postgres
+pg_dump -cO > postgres.sql
+cd $TEMP_DIR
+zip -r -u $FILE_NAME postgres
+
+# Delete temp folder
