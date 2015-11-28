043b678cc6294925447e1c109bcce141fa14aade
Truong <me@truongtx.me>
2015-11-28 22:18:17 +0700

    basic script for backup folders

---
+++ ansible/templates/backup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CURRENT_TIME="$(date +%Y-%m-%d-%H-%M-%S)"
+FILE_EXT="zip"
+FILE_NAME="{{ project_dir }}/backup/backup-on-$CURRENT_TIME.$FILE_EXT"
+
+DIRS=(
+    "{{ log_dir }}"
+    "{{ password_dir }}"
+)
+
+for path in "${DIRS[@]}"
+do
+    dir=$(basename $path)
+    cd $path/..
+    zip -r -u $FILE_NAME $dir
+done
