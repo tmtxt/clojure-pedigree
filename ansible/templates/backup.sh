2dfb90fb1c1f0bcae9e96ecd113cf523a5df22e2
Truong <me@truongtx.me>
2015-11-29 09:55:02 +0700

    better backup script

--- ansible/templates/backup.sh
+++ ansible/templates/backup.sh
@@ -2,26 +2,28 @@
 
 # Create temp folder
 TEMP_DIR="{{ project_dir }}/temp"
+BACKUP_DIR=$TEMP_DIR/backup
 mkdir -p $TEMP_DIR
+rm -rf $BACKUP_DIR
+mkdir -p $BACKUP_DIR
 
-# Backup folders
+# Backup file name
 CURRENT_TIME="$(date +%Y-%m-%d-%H-%M-%S)"
 FILE_EXT="zip"
 FILE_NAME="{{ project_dir }}/backup/backup-on-$CURRENT_TIME.$FILE_EXT"
 
-DIRS=(
-    {{ log_dir }}
-    {{ password_dir }}
-    {{ person_image_dir }}
-    {{ neo4j_path }}
+# folder to backup
+declare -A DIRS=(
+    [logs]={{ log_dir }}
+    [password]={{ password_dir }}
+    [person-image]={{ person_image_dir }}
+    [neo4j]={{ neo4j_path }}
 )
 
-for dir_path in "${DIRS[@]}"
-do
-    cd "$dir_path"
-    cd ..
-    dir=$(basename "$dir_path")
-    zip -r -u $FILE_NAME $dir
+# create zip structure
+for k in "${!DIRS[@]}"; do
+    mkdir $BACKUP_DIR/$k
+    cp -R ${DIRS[$k]}/* $BACKUP_DIR/$k
 done
 
 # Backup postgres database
@@ -31,11 +33,16 @@ export PGUSER={{ db_user }}
 export PGPASSWORD={{ db_password }}
 export PGPORT=5432
 
-mkdir -p $TEMP_DIR/postgres
-cd $TEMP_DIR/postgres
+mkdir -p $BACKUP_DIR/postgres
+cd $BACKUP_DIR/postgres
 pg_dump -cO > postgres.sql
-cd $TEMP_DIR
-zip -r -u $FILE_NAME postgres
 
-# Delete temp folder
-rm -rf $TEMP_DIR/postgres
+# create zip file
+cd $BACKUP_DIR
+for dir_path in $BACKUP_DIR/*; do
+    dir=$(basename "$dir_path")
+    zip -r -u $FILE_NAME $dir
+done
+
+# remove temp backup dir
+rm -rf $BACKUP_DIR
