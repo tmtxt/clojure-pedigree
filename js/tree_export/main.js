26f91fe76dc231bd01bca8fd1804727c1f18af91
Truong <me@truongtx.me>
2015-12-27 15:53:27 +0700

    temp

--- js/tree_export/main.js
+++ js/tree_export/main.js
@@ -1,1 +1,20 @@
-console.log('hello');
+var jquery = require('jquery');
+var Request = require('tree/request.js');
+
+var config = {};
+config.getPersonId = function() {
+  return 1;
+};
+config.getTreeDepth = function() {
+  return 5;
+};
+
+var page = {};
+page.config = config;
+
+var exportButton = jquery('.js-export-tree');
+exportButton.click(function(){
+  Request.getTreeData(page).then(function(data){
+    console.log(data);
+  });
+});
