3b634fb50a19f357c3b8900ea91eb0d9be19975e
Truong <me@truongtx.me>
2016-01-02 21:18:49 +0700

    add config for connecting neo node

--- ansible/templates/web/neo4j.clj
+++ ansible/templates/web/neo4j.clj
@@ -5,3 +5,6 @@
 
 ;; connects to a Neo4J Server with default username neo4j and password
 (def conn (nr/connect "http://{{ web_neo4j_host }}:{{ web_neo4j_port }}/db/data"))
+
+(def neonode-host {{ neo_node_host | to_json }})
+(def neonode-port {{ neo_node_port | to_json }})
