117b65d453451c2760ab241a1e7d9918de3b2520
Truong <me@truongtx.me>
2016-01-03 00:03:09 +0700

    change add person to use neo node api

--- site/src/app/models/person/add.clj
+++ site/src/app/models/person/add.clj
@@ -1,15 +1,14 @@
 (ns app.models.person.add
   (:require [app.models.person.definition :as definition]
             [korma.core :as kc]
-            [app.neo4j.main :as neo4j]
             [app.models.person.validation :as validation]
             [slingshot.slingshot :refer [throw+ try+]]
-            [app.neo4j.node :as node]))
+            [app.models.person.node :as node]))
 
 (defn add-person
   "Add new person into postgres and neo4j."
   [person-data & {:keys [is-root]
-              :or {is-root false}}]
+                  :or {is-root false}}]
   (let [errors (validation/validate-person-data person-data)]
     (when (-> errors empty? not)
       (throw+ {:type :pg-validation
@@ -17,10 +16,9 @@
                :errors errors})))
   (let [person-entity (kc/insert definition/person
                                  (kc/values person-data))
-        person-node (node/create-or-update
-                     :person
-                     {:person_id (:id person-entity)}
-                     {:is_root is-root})]
+        person-node (node/add-or-update
+                     {:person_id (:id person-entity)
+                      :is_root is-root})]
     {:success true
      :entity person-entity
      :node person-node}))
