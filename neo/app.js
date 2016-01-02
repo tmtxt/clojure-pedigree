2b43f36947fa4e32241885e9dbb78e4ab37c138b
Truong <me@truongtx.me>
2016-01-02 16:03:45 +0700

    add route for person

--- neo/app.js
+++ neo/app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+var person = require('./person');
 
 // config
 var config = require('./config/main.js');
 app.set('config', config);
 
+// routes
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.use('/person', person);
 
 var port = config.get('serverPort');
 var server = app.listen(port, function () {
