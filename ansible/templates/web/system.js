acecf6213af9c70879a69e399ce8514527af4fd8
Truong <me@truongtx.me>
2016-01-02 19:47:25 +0700

    link neo4j to neo node

--- ansible/templates/web/system.js
+++ ansible/templates/web/system.js
@@ -1,5 +1,7 @@
 var config = new Map([
-  ['serverPort', {{ neo_node_port }}]
+  ['serverPort', {{ neo_node_port }}],
+  ['neo4jHost', {{ neo_neo4j_host | to_json }}],
+  ['neo4jPort', {{ neo_neo4j_port }}]
 ]);
 
 module.exports = config;
