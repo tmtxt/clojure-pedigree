4a4e2f7098be59bd45f1f852768522bf7245b21b
Truong <me@truongtx.me>
2016-01-02 15:54:19 +0700

    remove un-used code

--- neo/app.js
+++ neo/app.js
@@ -10,9 +10,7 @@ app.get('/', function (req, res) {
 });
 
 var port = config.get('serverPort');
-
 var server = app.listen(port, function () {
-  var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at %s', port);
 });
