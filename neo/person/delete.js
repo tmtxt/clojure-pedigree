57d3fa40250cec0b8cf3322aa2bac43e7af80c6f
Truong <me@truongtx.me>
2016-01-03 00:14:45 +0700

    add delete api for neo node

---
+++ neo/person/delete.js
@@ -0,0 +1,48 @@
+function findMatching(neo4j, personId) {
+  return new Promise(function(resolve, reject) {
+    neo4j.find({person_id: personId}, 'person', function(err, persons){
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      if (persons.length !== 0) {
+        resolve(persons[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+
+function deleteNode(neo4j, node) {
+  return new Promise(function(resolve, reject){
+    if (!node) {
+      resolve();
+      return;
+    }
+
+    neo4j.delete(node, function(err){
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function deleteHandler(req, res, next) {
+  var app = req.app;
+  var neo4j = app.get('neo4j');
+  var personId = req.body.personId;
+
+  findMatching(neo4j, personId).then(function(person){
+    return deleteNode(neo4j, person);
+  }).then(function(){
+    res.json({success: true});
+  }).catch(function(){
+    res.json({success: false});
+  });
+}
+exports.deleteHandler = deleteHandler;
--- neo/person/index.js
+++ neo/person/index.js
@@ -3,17 +3,20 @@ var router = express.Router();
 
 var find = require('./find.js');
 var add = require('./add.js');
+var deletePerson = require('./delete.js');
 var util = require('./util.js');
 
 router.use('/findPerson', util.requirePersonId);
 router.use('/findParents', util.requirePersonId);
 router.use('/findPartners', util.requirePersonId);
-
 router.get('/findRoot', find.findRootHandler);
 router.get('/findPerson', find.findPersonHandler);
 router.get('/findPartners', find.findPartnersHandler);
 router.get('/findParents', find.findParentsHandler);
 
 router.post('/addOrUpdate', add.addOrUpdateHandler);
+
+router.use('/delete', util.requirePersonId);
+router.post('/delete', deletePerson.deleteHandler);
 
 module.exports = router;
