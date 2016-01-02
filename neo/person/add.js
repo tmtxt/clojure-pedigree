8b025a47c8d4730c9e660dd4bd704a73e252a728
Truong <me@truongtx.me>
2016-01-02 23:49:51 +0700

    add insert person node api for neo node

---
+++ neo/person/add.js
@@ -0,0 +1,61 @@
+var _ = require('lodash');
+
+function findMatching(neo4j, data) {
+  return new Promise(function(resolve, reject) {
+    if (!!data.person_id) {
+      neo4j.find({person_id: data.person_id}, 'person', function(err, persons){
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        if (persons.length !== 0) {
+          resolve(persons[0]);
+        } else {
+          resolve(null);
+        }
+      });
+    } else {
+      resolve(null);
+    }
+  });
+}
+
+function addOrUpdate(neo4j, data) {
+  return new Promise(function(resolve, reject){
+    var handler = function(err){
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    };
+
+    if (!!data.id) {
+      neo4j.save(data, handler);
+    } else {
+      neo4j.save(data, 'person', handler);
+    }
+  });
+}
+
+function addOrUpdateHandler(req, res, next) {
+  var app = req.app;
+  var neo4j = app.get('neo4j');
+  var data = req.body.data;
+
+  if (!data) {
+    res.json({success: false});
+    return;
+  }
+
+  findMatching(neo4j, data).then(function(person){
+    if (!!person) data = _.extend(person, data);
+    return addOrUpdate(neo4j, data);
+  }).then(function(){
+    res.json({success: true});
+  }).catch(function(){
+    res.json({success: false});
+  });
+}
+exports.addOrUpdateHandler = addOrUpdateHandler;
--- neo/person/index.js
+++ neo/person/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var find = require('./find.js');
+var add = require('./add.js');
 var util = require('./util.js');
 
 router.use('/findPerson', util.requirePersonId);
@@ -12,5 +13,7 @@ router.get('/findRoot', find.findRootHandler);
 router.get('/findPerson', find.findPersonHandler);
 router.get('/findPartners', find.findPartnersHandler);
 router.get('/findParents', find.findParentsHandler);
+
+router.post('/addOrUpdate', add.addOrUpdateHandler);
 
 module.exports = router;
