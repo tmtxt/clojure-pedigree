0b208a3a9156f9c84cbad13679906f66bcc7422c
Truong <me@truongtx.me>
2016-01-04 21:35:43 +0700

    update add from child for neo node

--- site/src/app/controllers/person/add/from_child.clj
+++ site/src/app/controllers/person/add/from_child.clj
@@ -8,25 +8,21 @@
             [app.models.pedigree-relation :as prl]))
 
 (defn process-get-request [request]
-  (neo4j/with-transaction
-    (let [child (controller-util/find-person-from-request request "childId")]
-      (if (and child (-> child person/enough-parents? not))
-        (render/render-add-page request {:action "add"
-                                         :from "child"
-                                         :child child})
-        (render/render-add-page request))
-      )))
+  (let [child (controller-util/find-person-from-request request "childId")]
+    (if (and child (-> child person/enough-parents? not))
+      (render/render-add-page request {:action "add"
+                                       :from "child"
+                                       :child child})
+      "enough parent")
+    ))
 
 (defn process-post-request [request]
-  (neo4j/with-transaction
-    (kd/transaction
-     (if-let [child (controller-util/find-person-from-request request :childId)]
-       (let [person-result (controller-util/create-person-from-request request)
-             person-entity (person-result :entity)
-             person-node (person-result :node)
-             child-node (person/find-node-by-person-id (:id child))
-             parent-role (person-util/determine-father-mother-single person-entity)]
-         (if (= parent-role :father)
-           (prl/add-child-for-father person-node child-node)
-           (prl/add-child-for-mother person-node child-node)))
-       "child nil"))))
+  (kd/transaction
+   (if-let [child (controller-util/find-person-from-request request :childId)]
+     (let [person-result (controller-util/create-person-from-request request)
+           person-entity (person-result :entity)
+           parent-role (person-util/determine-father-mother-single person-entity)]
+       (if (= parent-role :father)
+         (prl/add-child-for-father person-entity child)
+         (prl/add-child-for-mother person-entity child)))
+     "child nil")))
