2460753c33e1d41b9ed2633da99df937e20d51ff
Truong <me@truongtx.me>
2016-01-04 20:54:26 +0700

    fix person find controller with neo node

--- site/src/app/controllers/person/find.clj
+++ site/src/app/controllers/person/find.clj
@@ -6,35 +6,34 @@
             [app.util.main :as util]))
 
 (defn find-list-simple [request]
-  (neo4j/with-transaction
-    (let [{parent-id :parentId
-           term :term
-           parent-role :parentRole} (util/params request)]
-      (cond
-        ;; TODO update this later
-        (and parent-id term)
-        (let [person-list (-> term person/find-entity-by-full-name person-util/filter-persons-keys)]
-          (response person-list))
+  (let [{parent-id :parentId
+         term :term
+         parent-role :parentRole} (util/params request)]
+    (cond
+      ;; TODO update this later
+      (and parent-id term)
+      (let [person-list (-> term person/find-entity-by-full-name person-util/filter-persons-keys)]
+        (response person-list))
 
-        ;; TODO update this later
-        (and parent-role term)
-        (let [person-list (-> term person/find-entity-by-full-name person-util/filter-persons-keys)]
-          (response person-list))
+      ;; TODO update this later
+      (and parent-role term)
+      (let [person-list (-> term person/find-entity-by-full-name person-util/filter-persons-keys)]
+        (response person-list))
 
-        parent-role
-        (let [role-genders (person-util/parent-role-genders parent-role)
-              person-list (-> role-genders person/find-entities-by-genders person-util/filter-persons-keys)]
-          (response person-list))
+      parent-role
+      (let [role-genders (person-util/parent-role-genders parent-role)
+            person-list (-> role-genders person/find-entities-by-genders person-util/filter-persons-keys)]
+        (response person-list))
 
-        parent-id
-        (let [partners (-> {:id parent-id} person/find-partners-of-entity person-util/filter-persons-keys)]
-          (response partners))
+      parent-id
+      (let [parent-id (util/parse-int parent-id)
+            partners (-> {:id parent-id} person/find-partners-of-entity person-util/filter-persons-keys)]
+        (response partners))
 
-        term
-        (let [all (-> term person/find-entities-by-full-name person-util/filter-persons-keys)]
-          (response all))
+      term
+      (let [all (-> term person/find-entities-by-full-name person-util/filter-persons-keys)]
+        (response all))
 
-        :else
-        (response [])
-        ))
-    ))
+      :else
+      (response [])
+      )))
