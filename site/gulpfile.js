de0fa580a96180dd913148d966413549d5000712
Tran Xuan Truong <truong.tran@lazada.com>
2016-01-05 15:43:07 +0700

    not generate version using gulp

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -4,9 +4,8 @@ var gulp = require('gulp');
 require('./gulp/bower.js');
 require('./gulp/browserify.js');
 require('./gulp/sass.js');
-require('./gulp/version.js');
 
 // combine
-gulp.task('dev', ['bower', 'js-dev', 'sass-dev', 'update-version']);
-gulp.task('prod', ['bower', 'js-prod', 'sass-dev', 'update-version']);
+gulp.task('dev', ['bower', 'js-dev', 'sass-dev']);
+gulp.task('prod', ['bower', 'js-prod', 'sass-dev']);
 gulp.task('watch', ['js-watch', 'sass-watch']);
