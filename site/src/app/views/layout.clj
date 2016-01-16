9edaa176cba6c75f139d58ec8304f67aa646bdc7
Truong <me@truongtx.me>
2016-01-16 14:53:40 +0700

    layout for message page

--- site/src/app/views/layout.clj
+++ site/src/app/views/layout.clj
@@ -25,14 +25,22 @@
     (utf-8-response (parser/render-file template template-params))))
 
 (def alert-type {:success "success"
-                 :info "info"
-                 :warning "warning"
+                 :info "warning"
+                 :warning "danger"
                  :danger "danger"
                  :error "danger"})
 
+(def alert-title {:success "Thành công"
+                  :info "Thông báo"
+                  :warning "Thông tin"
+                  :danger "Lỗi"
+                  :error "Lỗi"})
+
 ;;; render message page
 (defn render-message [request message & {:keys [type]
-                                 :or {type :info}}]
-  (let [alert-class (get alert-type type "info")]
+                                         :or {type :info}}]
+  (let [class (get alert-type type "warning")
+        title (get alert-title type "Thông tin")]
     (render request "layouts/message.html" {:message message
-                                            :alert-class alert-class})))
+                                            :class class
+                                            :title title})))
