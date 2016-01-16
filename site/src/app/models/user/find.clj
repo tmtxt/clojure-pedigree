f3fc47d0df68cdf0238a81dd7d48c4bc0336689e
Truong <me@truongtx.me>
2016-01-16 22:00:37 +0700

    find ns for user model

---
+++ site/src/app/models/user/find.clj
@@ -0,0 +1,12 @@
+(ns app.models.user.find
+  (:require [korma.core :refer [select with where]]
+            [app.models.user.definition :refer [user]]
+            [app.models.user-role :refer [user-role]]
+            [slingshot.slingshot :refer [throw+ try+]]))
+
+(defn find-by-username [username]
+  (-> user
+      (select
+       (with user-role)
+       (where {:username username}))
+      first))
