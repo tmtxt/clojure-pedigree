c23d0b1780b6e4d26461ae282aa7da137a668f67
Truong <me@truongtx.me>
2016-01-16 16:21:01 +0700

    basic admin template

--- site/src/app/controllers/admin.clj
+++ site/src/app/controllers/admin.clj
@@ -1,14 +1,20 @@
 (ns app.controllers.admin
   (:require [compojure.core :refer :all]
+            [environ.core :refer [env]]
+            [app.views.layout :refer [render]]
             [app.util.security :refer [user-access admin-access]]))
 
 (defn view-index [request]
-  "Admin index")
+  (render request "admin/index.html"))
 
 (def admin-routes
   (context
    "/admin" []
    (GET "/index" [] view-index)))
 
-(def admin-rules [{:pattern #"^/admin/.*"
-                  :handler {:and [user-access admin-access]}}])
+(def admin-rules
+  (if (-> :profile env (= "dev"))
+    [{:pattern #"^/admin/sfkjdslkjfldjfs.*"
+      :handler admin-access}]
+    [{:pattern #"^/admin/.*"
+                  :handler {:and [user-access admin-access]}}]))
