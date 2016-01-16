a33cd0bf6a4b8b58fb9ec0917d7dac371d7dc24f
Truong <me@truongtx.me>
2016-01-16 21:14:53 +0700

    add password ns for user model

---
+++ site/src/app/models/user/password.clj
@@ -0,0 +1,15 @@
+(ns app.models.user.password
+  (:require [crypto.password.bcrypt :as crypto]))
+
+(defn hash-password
+  "Hash the input password"
+  [password]
+  (crypto/encrypt password))
+
+(defn hash-password-for-entity
+  "Hash password for the entity, return the new entity with the :password hashed if exists"
+  [entity]
+  (let [password (:password entity)]
+    (if (-> password nil? not)
+      (assoc entity :password (hash-password password))
+      entity)))
