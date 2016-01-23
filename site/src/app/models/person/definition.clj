3ef91ec2ab45ee584367f8accdbc50da81bb7833
Truong <me@truongtx.me>
2016-01-23 22:09:06 +0700

    refactor person model definition

--- site/src/app/models/person/definition.clj
+++ site/src/app/models/person/definition.clj
@@ -1,69 +1,48 @@
 (ns app.models.person.definition
-  (:require [app.util.pg :as db-util]
-            [korma.core :as kc]
-            [camel-snake-kebab.core :refer :all]
-            [app.models.person.util :as model-util]
-            [app.models.person.prepare :as prepare]
-            [camel-snake-kebab.extras :refer [transform-keys]]
-            [config.main :refer [config]]))
+  (:require [korma.core :refer [defentity table pk transform prepare]]
+            [app.models.person.prepare :refer [prepare-data]]
+            [app.models.person.transform :refer [transform-data]]
+            [app.util.model :refer [entity->record record->entity]]))
 
-;;; custom-defined record
-;;; for postgres result
 (defrecord PersonEntity
-    [id
-     full-name
-     address
-     phone-no
-     summary
-     gender
-     alive-status
-     death-date
-     birth-date
-     picture
-     created-at
-     job])
+    [id full-name address phone-no summary gender alive-status
+     death-date birth-date picture created-at job])
 
-;;; for neo4j result
 (defrecord PersonNode
     [person-id is-root])
+
+(defn- transform-func
+  [data]
+  (-> data (entity->record map->PersonEntity) transform-data))
+
+(defn- prepare-func
+  [data]
+  (-> data prepare-data record->entity))
+
+(defentity person
+  (table :tbl_person)
+  (pk :id)
+  (transform transform-func)
+  (prepare prepare-func))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; TODO remove these
+(defn entity-to-record
+  "Convert korma entity to PersonEntity record"
+  [entity]
+  entity)
+
+(defn node-to-record
+  "Convert node to PersonNode record"
+  [node]
+  node)
+
+(defn record-to-node
+  "Convert PersonNode record back to node"
+  [record]
+  record)
 
 ;;; props list
 (def EntityProps (PersonEntity/getBasis))
 
 (def NodeProps (PersonNode/getBasis))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn entity-to-record
-  "Convert korma entity to PersonEntity record"
-  [entity]
-  (let [entity (transform-keys ->kebab-case entity)]
-    (map->PersonEntity entity)))
-
-;;; transform data from pg object to clojure data when select
-(defn- transform-data [data]
-  (let [data (entity-to-record data)    ;convert to record first
-        {picture :picture} data         ;destruct the data
-        picture (if picture picture (:default-person-image config)) ;process the data
-        ]
-    ;; assoc back the data
-    (assoc data
-           :picture picture)
-    ))
-
-;;; korma entity
-(kc/defentity person
-  (kc/table :tbl_person)
-  (kc/pk :id)
-  (kc/transform transform-data)
-  (kc/prepare prepare/prepare-data))
-
-(defn node-to-record
-  "Convert node to PersonNode record"
-  [node]
-  (let [node (transform-keys ->kebab-case node)]
-    (map->PersonNode node)))
-
-(defn record-to-node
-  "Convert PersonNode record back to node"
-  [record]
-  (transform-keys ->snake_case record))
---
+++ site/src/app/models/person/transform.clj
@@ -0,0 +1,5 @@
+(ns app.models.person.transform)
+
+(defn transform-data
+  "Transform the record after querying from database"
+  [data] data)
