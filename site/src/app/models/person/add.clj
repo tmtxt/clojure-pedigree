3a61dad8aab416017b932d67b02d8921c032b4b4
Truong <me@truongtx.me>
2016-01-23 22:19:30 +0700

    refactor person model add

--- site/src/app/models/person/add.clj
+++ site/src/app/models/person/add.clj
@@ -1,24 +1,26 @@
 (ns app.models.person.add
-  (:require [app.models.person.definition :as definition]
-            [korma.core :as kc]
-            [app.models.person.validation :as validation]
+  (:require [app.models.person.definition :refer [person]]
+            [korma.core :refer [insert values]]
             [slingshot.slingshot :refer [throw+ try+]]
-            [app.models.person.node :as node]))
+            [app.models.person.node :refer [add-or-update] :rename {add-or-update add-node}]))
+
+(defn- add-person-entity
+  "Add person entity to Postgres"
+  [data]
+  (insert person (values data)))
+
+(defn- add-person-node
+  "Add person node in Neo4j for person entity"
+  [entity is-root]
+  (add-node {:person_id (:id entity)
+             :is_root is-root}))
 
 (defn add-person
   "Add new person into postgres and neo4j."
-  [person-data & {:keys [is-root]
-                  :or {is-root false}}]
-  (let [errors (validation/validate-person-data person-data)]
-    (when (-> errors empty? not)
-      (throw+ {:type :pg-validation
-               :success false
-               :errors errors})))
-  (let [person-entity (kc/insert definition/person
-                                 (kc/values person-data))
-        person-node (node/add-or-update
-                     {:person_id (:id person-entity)
-                      :is_root is-root})]
+  [data & {:keys [is-root]
+           :or {is-root false}}]
+  (let [entity (add-person-entity data)
+        node (add-person-node entity is-root)]
     {:success true
-     :entity person-entity
-     :node person-node}))
+     :entity entity
+     :node node}))
