0b89aef1ed07c74537ab0436c4bf0d154b430ff9
Truong <me@truongtx.me>
2016-01-24 10:00:49 +0700

    refactor pedigree relation

--- site/src/app/models/pedigree_relation.clj
+++ site/src/app/models/pedigree_relation.clj
@@ -1,35 +1,29 @@
 (ns app.models.pedigree-relation
-  (:require [app.neo4j.main :as neo4j]))
+  (:require [app.neo4j.main :as neo4j]
+            [camel-snake-kebab.core :refer [->snake_case]]))
 
-(def RELATION_TYPES
-  {:father-child :father_child
-   :mother-child :mother_child})
-
-(defn add-relation-from-node
-  "Add new relation between two node in the system"
-  [parent-entity child-entity order & {:keys [type]
-                                       :or [type (:father-child RELATION_TYPES)]}]
-  (neo4j/neonode :post "/pedigree/addChildForParent"
-                 {:parentId (:id parent-entity)
-                  :childId (:id child-entity)
-                  :type type
-                  :order order}))
+(defn- link-persons
+  "Link two persons entity"
+  [parent-entity child-entity rel-type]
+  (let [rel-type (->snake_case rel-type)]
+    (neo4j/neonode :post "/pedigree/addChildForParent"
+                   {:parentId (:id parent-entity)
+                    :childId (:id child-entity)
+                    :type rel-type
+                    :order 0})))
 
 (defn add-child
-  "Add child for the given parents node"
-  [father-entity mother-entity child-entity & [order]]
-  (let [_order (if order order 0)]
-    (add-relation-from-node father-entity child-entity _order
-                            :type (:father-child RELATION_TYPES))
-    (add-relation-from-node mother-entity child-entity _order
-                            :type (:mother-child RELATION_TYPES))))
+  "Link a child entity with father and mother entity"
+  [father-entity mother-entity child-entity]
+  (link-persons father-entity child-entity :father-child)
+  (link-persons mother-entity child-entity :mother-child))
 
-(defn add-child-for-mother [mother-entity child-entity & [order]]
-  (let [_order (if order order 0)]
-    (add-relation-from-node mother-entity child-entity _order
-                            :type (:mother-child RELATION_TYPES))))
+(defn add-child-for-mother
+  "Link a child entity with mother entity"
+  [mother-entity child-entity]
+  (link-persons mother-entity child-entity :mother-child))
 
-(defn add-child-for-father [father-entity child-entity & [order]]
-  (let [_order (if order order 0)]
-    (add-relation-from-node father-entity child-entity _order
-                            :type (:father-child RELATION_TYPES))))
+(defn add-child-for-father
+  "Link a child entity with father entity"
+  [father-entity child-entity]
+  (link-persons father-entity child-entity :father-child))
