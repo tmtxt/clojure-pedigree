7a2267f18135b58bf04be0fbfc33f07898b304ef
Truong <me@truongtx.me>
2016-04-02 18:33:16 +0700

    install winston

--- svc.models/app.js
+++ svc.models/app.js
@@ -3,12 +3,20 @@ var app = require('koa')();
 var config = require('./config');
 var parser = require('koa-bodyparser');
 var json = require('koa-json');
+var winston = require('winston');
 
 var persons = require('./routes/persons.js');
 
 // global middleware
 app.use(parser());
 app.use(json());
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
 
 // routes definition
 koa.use('/persons', persons.routes(), persons.allowedMethods());
