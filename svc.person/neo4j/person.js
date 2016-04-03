18929250878d606ad864fa483a3947223e9d4ab2
Truong <me@truongtx.me>
2016-04-03 23:59:49 +0700

    function for adding person node

---
+++ svc.person/neo4j/person.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var neo = require('./db.js');
+
+/**
+ * Save person node to neo4j
+ *
+ * @param  {object} person The person model
+ * @param {boolean} isRoot Whether this person is root or not
+ * @return {object} The newly inserted node
+ */
+exports.save = function(person, isRoot) {
+  var data = {
+    person_id: person.id,
+    is_root: isRoot
+  };
+
+  var promise = new Promise(function(resolve, reject){
+    neo.save(data, 'person', function(err, node) {
+      console.log(err);
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(node);
+    });
+  });
+
+  return promise;
+};
--- svc.person/routes/add.js
+++ svc.person/routes/add.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('koa-router')();
+const _ = require('lodash');
 
 // Validate Pg model
 function* validateModel(person, logTrace) {
@@ -14,8 +15,14 @@ function* validateModel(person, logTrace) {
 
 // Save pg model
 function* savePgModel(person, transaction, logTrace) {
-  yield person.save({transaction});
   logTrace.add('info', 'savePgModel()', 'Temporary save pg model');
+  yield person.save({transaction});
+}
+
+// Add person node in neo4j
+function* addNode(person, isRoot, neoPerson, logTrace) {
+  logTrace.add('info', 'addNode()', 'Saving person node to neo4j');
+  yield neoPerson.save(person, isRoot);
 }
 
 // Koa handler function
@@ -23,12 +30,16 @@ function* addHandler() {
   const logTrace = this.logTrace;
   const Person = this.pg.Person;
   var db = this.pg.db;
+  var neoPerson = this.neo.person;
   const transaction = yield db.transaction();
+  const personData = this.request.body;
+  const isRoot = personData.isRoot || false;
 
   try {
-    const person = Person.build(this.request.body);
+    const person = Person.build(personData);
     yield validateModel(person, logTrace);
     yield savePgModel(person, transaction, logTrace);
+    yield addNode(person, isRoot, neoPerson, logTrace);
 
     yield transaction.commit();
     logTrace.add('info', 'transaction.commit()');
@@ -38,7 +49,7 @@ function* addHandler() {
     };
   } catch (err) {
     yield transaction.rollback();
-    logTrace.add('error', 'transaction.rollback()');
+    logTrace.add('error', 'transaction.rollback()', err);
     this.body = {
       success: false,
       message: err
