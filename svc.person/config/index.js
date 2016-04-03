318ee63f5be6afed1da7e1ba447f0631135774b1
Truong <me@truongtx.me>
2016-04-03 12:18:51 +0700

    basic config for connecting postgres

--- svc.person/config/index.js
+++ svc.person/config/index.js
@@ -1,3 +1,9 @@
 module.exports = {
-  serverPort: process.env.SVC_MODELS_PORT
+  serverPort: process.env.SVC_MODELS_PORT,
+
+  postgresHost: process.env.POSTGRES_HOST,
+  postgresPort: process.env.POSTGRES_PORT,
+  postgresUser: process.env.POSTGRES_USER,
+  postgresPassword: process.env.POSTGRES_PASSWORD,
+  postgresDb: process.env.POSTGRES_DB
 };
