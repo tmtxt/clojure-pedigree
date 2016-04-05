609598ec64cd62cb5a1823651dec2efc3addff66
Truong <me@truongtx.me>
2016-04-05 21:55:38 +0700

    add route for find person by id

--- svc.person/routes/find.js
+++ svc.person/routes/find.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('koa-router')();
+const util = require('./util.js');
 
 function* findRootHandler() {
   const logTrace = this.logTrace;
@@ -47,6 +48,42 @@ function* findRootHandler() {
   }
 }
 
-router.post('/root', findRootHandler);
+function* findByIdHandler() {
+  var logTrace = this.logTrace;
+  var personId = this.request.body.personId;
+  var pgPerson = this.pg.Person;
+  var neoPerson = this.neo.person;
+
+  try {
+    logTrace.add('info', 'pgPerson.findById()');
+    const model = yield pgPerson.findById(personId);
+    if (!model) {
+      throw {message: `Cannot find model with id ${personId}`};
+    }
+
+    const node = yield neoPerson.find(personId);
+    if (!node) {
+      throw {message: `Cannot find node with id ${personId}`};
+    }
+
+    logTrace.add('info', `Person with id ${personId} found`);
+    this.body = {
+      success: true,
+      data: {
+        model: model.getData(),
+        node
+      }
+    };
+  } catch (err) {
+    logTrace.add('error', 'Find person by id fail', err);
+    this.body = {
+      success: false,
+      message: err.message || 'Find person by id fail'
+    };
+  }
+}
+
+router.get('/root', findRootHandler);
+router.get('/byId', util.requireIdMdw, findByIdHandler);
 
 module.exports = router;
