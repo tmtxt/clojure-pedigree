f19e257c16870d3fb5218668fa32e2b8d198dc5d
Truong <me@truongtx.me>
2016-04-07 23:52:36 +0700

    add handler for auth user

--- svc.user/app.js
+++ svc.user/app.js
@@ -18,9 +18,11 @@ var context = {
 var addUser = require('./routes/add_user.js');
 var findUser = require('./routes/find_user.js');
 var changePassword = require('./routes/change_password.js');
+var authUser = require('./routes/auth_user.js');
 koa.use('/user', addUser.routes(), addUser.allowedMethods());
 koa.use('/user', findUser.routes(), findUser.allowedMethods());
 koa.use('/user', changePassword.routes(), changePassword.allowedMethods());
+koa.use('/user', authUser.routes(), authUser.allowedMethods());
 var routes = koa.routes();
 
 // create the app
