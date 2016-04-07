266c76a0613616c7f3a2fe559c057b1a24dc85a1
Truong <me@truongtx.me>
2016-04-08 00:03:02 +0700

    remove svc web server

--- svc.web-server/env/prod/clj/svc/web_server/env.clj
+++
@@ -1,8 +0,0 @@
-(ns svc.web-server.env
-  (:require [clojure.tools.logging :as log]))
-
-(def defaults
-  {:init
-   (fn []
-     (log/info "\n-=[svc.web-server started successfully]=-"))
-   :middleware identity})
