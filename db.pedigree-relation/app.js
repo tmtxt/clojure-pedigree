3975a6f2c4f33c72040242a39fe09575f48a3117
Truong <me@truongtx.me>
2016-04-08 23:48:44 +0700

    handler for add pedigree relation from both

--- db.pedigree-relation/app.js
+++ db.pedigree-relation/app.js
@@ -17,8 +17,10 @@ var context = {
 // routes
 var addFromFather = require('./routes/add_from_father.js');
 var addFromMother = require('./routes/add_from_mother.js');
+var addFromBoth = require('./routes/add_from_both.js');
 koa.use('/add', addFromFather.routes(), addFromFather.allowedMethods());
 koa.use('/add', addFromMother.routes(), addFromMother.allowedMethods());
+koa.use('/add', addFromBoth.routes(), addFromBoth.allowedMethods());
 var routes = koa.routes();
 
 // create the app
