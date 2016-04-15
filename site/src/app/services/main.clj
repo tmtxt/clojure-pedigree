c339a3053abe508163a75e70a5bd32ec2911662d
Truong <me@truongtx.me>
2016-04-15 22:39:01 +0700

    remove main service file

--- site/src/app/services/main.clj
+++
@@ -1,5 +0,0 @@
-(ns app.services.main
-  (:require [config.services :refer [api-logic-host api-logic-port]]
-            [clj-http.client :as client]
-            [clojure.data.json :as json]
-            [slingshot.slingshot :refer [throw+]]))
