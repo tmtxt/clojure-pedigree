dd2ccd5d86a36850935eef819af8d5f80631e12d
Truong <me@truongtx.me>
2016-04-15 22:34:52 +0700

    function to call svc user auth

--- site/src/app/services/user.clj
+++ site/src/app/services/user.clj
@@ -1,4 +1,5 @@
 (ns app.services.user
   (:require [app.services.util :refer [call]]))
 
-(defn authenticate)
+(defn authenticate [username password]
+  (call :svc-user "/user/auth" :post {:username username :password password}))
