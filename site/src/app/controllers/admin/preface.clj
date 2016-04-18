c2664312a4e2b7f7e4a708184e2630eff4ed32cd
Truong <me@truongtx.me>
2016-04-18 14:11:20 +0700

    use new view render instead of old

--- site/src/app/controllers/admin/preface.clj
+++ site/src/app/controllers/admin/preface.clj
@@ -1,22 +1,18 @@
 (ns app.controllers.admin.preface
-  (:require [app.views.layout :refer [render render-message]]
+  (:require [app.views.main :as view]
             [app.util.main :as util]
-            [slingshot.slingshot :refer [try+ throw+]]
-            [validateur.validation :as vl]
-            [app.models.minor-content :refer [find-content update-content]]
-            [config.main :refer [config]]
             [app.logic.preface-content :as preface]))
 
 (defn preface-render [request & [message type]]
   (let [content (preface/get)
         content (if message (assoc content :message message) content)
         content (if type (assoc content :message-type type) content)]
-    (render request "admin/preface.html" content)))
+    (view/render-template request "admin/preface.html" content)))
 
 (defn preface-process [request]
   (let [content (util/param request "preface-content" "")
         result (preface/update content)]
     (if result
-      (render request "admin/preface.html" {:content content
+      (view/render-template request "admin/preface.html" {:content content
                                             :message "Cập nhật thành công"})
       (preface-render request "Cập nhật không thành công" "error"))))
