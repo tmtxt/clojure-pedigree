757844f71f6832ad9f0f191f66d0b6d5ebc8308c
Truong <me@truongtx.me>
2016-04-18 22:20:11 +0700

    add some method for svc person

--- site/src/app/services/person.clj
+++ site/src/app/services/person.clj
@@ -7,3 +7,15 @@
    (-> (call :svc-person "/find/byId" :get {:person-id person-id})
        (:data))
    (catch Object _ nil)))
+
+(defn find-root []
+  (try+
+   (-> (call :svc-person "/find/root" :get {})
+       (:data))
+   (catch Object _ nil)))
+
+(defn add [person]
+  (try+
+   (-> (call :svc-person "/add" :post person)
+       (:data))
+   (catch Object _ nil)))
