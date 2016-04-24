da5ecfe140a17950620be45b38d43c69d3b7d074
Truong <me@truongtx.me>
2016-04-24 15:11:22 +0700

    validation for add image

--- svc.image/config/index.js
+++ svc.image/config/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  serverPort: process.env.SVC_IMAGE_PORT
+  serverPort: process.env.SVC_IMAGE_PORT,
+  imageDir: '/data'
 };
--- svc.image/routes/add.js
+++ svc.image/routes/add.js
@@ -1,28 +1,62 @@
 'use strict';
 
 const router = require('koa-router')();
+const uuid = require('node-uuid');
+const _ = require('lodash');
 
+const types = ['person'];
+
+function* validateMdw(next) {
+  const logTrace = this.logTrace;
+  const type = _.get(this, ['request', 'body', 'type']);
+  const image = _.get(this, ['req', 'files', 'image']);
+
+  if (_.isNil(type) || !_.includes(types, type)) {
+    let message = 'Invalid image type';
+    logTrace.add('error', 'Image type validation', message);
+    this.body = {
+      success: false,
+      message
+    };
+    return;
+  }
+
+  if (_.isNil(image) || _.isNil(image.path)) {
+    let message = 'No image uploaded';
+    logTrace.add('error', 'Image file validation', message);
+    this.body = {
+      success: false,
+      message
+    };
+    return;
+  }
+
+  yield next;
+}
 
 // Koa handler function
 function* addHandler() {
   const logTrace = this.logTrace;
-  const User = this.pg.User;
+  // const User = this.pg.User;
 
-  logTrace.add('info', 'User.count()');
-  const count = yield User.count();
+  // logTrace.add('info', 'User.count()');
+  // const count = yield User.count();
 
-  let message = `Found ${count} users`;
-  logTrace.add('info', message);
+  // let message = `Found ${count} users`;
+  // logTrace.add('info', message);
 
-  const empty = count == 0;
+  // const empty = count == 0;
 
+  // this.body = {
+  //   success: true,
+  //   message,
+  //   data: empty
+  // };
   this.body = {
-    success: true,
-    message,
-    data: empty
+    success: true
   };
 }
 
-router.get('/', addHandler);
+router.post('/', validateMdw, addHandler);
 
 module.exports = router;
