58a3e22526621afdf9c572409e63c6a6636b25a4
Truong <me@truongtx.me>
2016-05-07 13:01:02 +0700

    json output fn for timbre logger

--- site/src/app/logger/logger.clj
+++ site/src/app/logger/logger.clj
@@ -1,9 +1,21 @@
 (ns app.logger.logger
   (:require [slingshot.slingshot :refer [try+ throw+]]
-            [taoensso.timbre :as timbre]))
+            [taoensso.timbre :as timbre]
+            [clojure.data.json :as json]))
+
+(defn json-output-fn
+  [{:keys [vargs_ hostname_ timestamp_ level] :as args}]
+  (let [messages (map (fn [msg] (merge msg
+                                      {:timestamp @timestamp_
+                                       :level     level
+                                       :hostname  @hostname_}))
+                      @vargs_)
+        json-messages (map #(json/write-str %) messages)]
+    (clojure.string/join "\n" json-messages)))
 
 (def timbre-config
   {:level :debug
+   :appenders {:println {:output-fn json-output-fn}}
    :timestamp-opts {:pattern "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", :locale :jvm-default, :timezone :utc}})
 
 (timbre/merge-config! timbre-config)
