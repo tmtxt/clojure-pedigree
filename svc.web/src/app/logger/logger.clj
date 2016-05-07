885d707123452c5de2f52f4357e377a76ab19832
Truong <me@truongtx.me>
2016-05-07 13:43:55 +0700

    add file appender for timbre

--- svc.web/src/app/logger/logger.clj
+++ svc.web/src/app/logger/logger.clj
@@ -1,9 +1,11 @@
 (ns app.logger.logger
   (:require [slingshot.slingshot :refer [try+ throw+]]
             [taoensso.timbre :as timbre]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [taoensso.timbre.appenders.core :as appenders]))
 
 (defn json-output-fn
+  "Jsonify output function for file appender"
   [{:keys [vargs_ hostname_ timestamp_ level] :as args}]
   (let [messages (map (fn [msg] (merge msg
                                       {:timestamp @timestamp_
@@ -15,7 +17,11 @@
 
 (def timbre-config
   {:level :debug
-   :appenders {:println {:output-fn json-output-fn}}
-   :timestamp-opts {:pattern "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", :locale :jvm-default, :timezone :utc}})
+   :appenders {:println {:enable? true}
+               :spit (merge (appenders/spit-appender {:fname "/logs/svc.web.log"})
+                            {:output-fn json-output-fn})}
+   :timestamp-opts {:pattern "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+                    :locale :jvm-default
+                    :timezone :utc}})
 
 (timbre/merge-config! timbre-config)
