50ff79dc98febc1057e6a1cf22c5e419e2037e33
Truong <me@truongtx.me>
2016-05-21 14:00:24 +0700

    different file for dev and prod

---
+++ set-env.prod.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# aliases
+alias dcu="docker-compose -f docker-compose.yml -f docker-compose.prod.yml up"
+alias dcud="docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d"
+alias dcp="docker-compose -f docker-compose.yml -f docker-compose.prod.yml ps"
+alias dcr="docker-compose -f docker-compose.yml -f docker-compose.prod.yml rm"
+alias dcl="docker-compose -f docker-compose.yml -f docker-compose.prod.yml logs"
+alias dcb="docker-compose -f docker-compose.yml -f docker-compose.prod.yml build"
+
+function attach {
+    docker exec -it $(docker-compose -f docker-compose.yml -f docker-compose.prod.yml ps | grep "$1" | awk '{print $1}') /bin/bash
+}
+
+function kill_and_run {
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml kill $1
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml up $1
+}
+
+function svc.web {
+    kill_and_run "svc.web"
+}
+
+function svc.person {
+    kill_and_run "svc.person"
+}
+
+function svc.tree {
+    kill_and_run "svc.tree"
+}
+
+function svc.api-tree {
+    kill_and_run "svc.api-tree"
+}
