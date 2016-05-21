a12a0e8f2b651061d5108ba2e32717a58a71fb8c
Truong <me@truongtx.me>
2016-05-21 11:38:02 +0700

    remove prod tag

--- setup_prod.sh
+++ setup_prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-ansible-playbook -i "localhost," -c local --extra-vars '{"project_dir": "/home/pedigree/clojure-pedigree"}' --tags "prod" ansible/main.yml
+ansible-playbook -i "localhost," -c local --extra-vars '{"project_dir": "/home/pedigree/clojure-pedigree"}' ansible/main.yml
