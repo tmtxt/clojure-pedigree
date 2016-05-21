ce63f7f44ec78d1dfbe82dc716e618c47890233d
Truong <me@truongtx.me>
2016-05-22 01:00:16 +0700

    add more log for add from partner

--- svc.web/src/app/controllers/person/add/from_partner.clj
+++ svc.web/src/app/controllers/person/add/from_partner.clj
@@ -3,7 +3,8 @@
             [slingshot.slingshot :refer [try+ throw+]]
             [ring.util.response :refer [redirect]]
             [app.services.marriage-relation :as svc-mr]
-            [app.controllers.person.add.render :as render]))
+            [app.controllers.person.add.render :as render]
+            [app.logger.log-trace :as log-trace]))
 
 (defn process-get-request [request]
   (try+
@@ -17,15 +18,34 @@
 
 (defn process-post-request [request]
   (try+
-   (let [{partner-node   :node
+   (let [;; find partner (to add from) from the request
+         _ (log-trace/add :info "Find partner")
+         {partner-node   :node
           partner-entity :entity} (find-person request "partnerId")
+         _ (log-trace/add :info "Partner entity id" (:id partner-entity))
+         _ (log-trace/add :info "Partner node id" (:id partner-node))
+
+         ;; create new person from request
+         _ (log-trace/add :info "Create new person")
          {person-node   :node
           person-entity :entity}  (create-person request)
-         partner-role             (svc-mr/detect-partner-role-single partner-entity)]
+         _ (log-trace/add :info "Person entity id" (:id person-entity))
+         _ (log-trace/add :info "Person node id" (:id person-node))
+
+         ;; detect partner role
+         partner-role             (svc-mr/detect-partner-role-single partner-entity)
+         _ (log-trace/add :info "Partner role" partner-role)
+         ]
+
+     ;; add relation between 2 nodes
+     (log-trace/add :info "Add relation for two nodes")
      (if (= partner-role "husband")
        (svc-mr/add-relation partner-node person-node)
        (svc-mr/add-relation person-node  partner-node))
+
+     ;; redirect to person detail page
      (->> (:id person-entity)
           (str "/person/detail/")
           redirect))
+
    (catch Object _ (render/error-page request))))
