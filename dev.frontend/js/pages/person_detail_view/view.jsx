d6659caad5c4f28d11c94e410257e192ebdd8215
Truong <me@truongtx.me>
2016-06-05 17:34:58 +0700

    basic display for person detail

--- dev.frontend/js/pages/person_detail_view/view.jsx
+++ dev.frontend/js/pages/person_detail_view/view.jsx
@@ -5,8 +5,70 @@ const React = require('react');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div>
-        {this.props.params.personId}
+      <div className="page-persondetail">
+        <div className="persondetail-header">
+          <div className="persondetail-title">
+            Full Name
+          </div>
+
+          <div className="persondetail-buttons">
+            <a href="/tree/view/person/15" className="btn btn-info">Xem cây gia phả</a>
+          </div>
+        </div>
+
+        <div className="persondetail-body">
+          <div className="persondetail-col-1">
+            <img className="img-responsive img-thumbnail" alt="" src="" />
+          </div>
+
+          <div className="persondetail-col-2">
+            <div className="persondetail-profile">
+              <div className="profile-header">
+                Thông tin
+              </div>
+
+              <div className="profile-body">
+                <div className="profile-body-row">
+                  <div className="profile-body-left">
+                    Họ và Tên
+                  </div>
+                  <div className="profile-body-right">
+                    Full name
+                  </div>
+                </div>
+
+                <div className="profile-body-row">
+                  <div className="profile-body-left">
+                    Ngày sinh
+                  </div>
+                  <div className="profile-body-right">
+                    Birthdate
+                  </div>
+                </div>
+
+                <div className="profile-body-row">
+                  <div className="profile-body-left">
+                    Tình trạng
+                  </div>
+                  <div className="profile-body-right">
+                    Alive
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="persondetail-history">
+              <div className="history-header">
+                Chú thích
+              </div>
+              <div className="js-persondetail-history history-body">
+              </div>
+            </div>
+          </div>
+
+          <div className="persondetail-col-3">
+          </div>
+        </div>
       </div>
     );
   }
