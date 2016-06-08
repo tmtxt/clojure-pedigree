9692411191bdd717b0e8aed92d05da29a9f531b9
Truong <me@truongtx.me>
2016-06-08 23:55:40 +0700

    handle select picture

--- dev.frontend/js/pages/person_add_view/view/col1_view.jsx
+++ dev.frontend/js/pages/person_add_view/view/col1_view.jsx
@@ -1,22 +1,52 @@
 'use strict';
 
+const $ = require('jquery');
 const React = require('react');
+const {Component} = React;
 
-module.exports = React.createClass({
-  render: function() {
+module.exports = class Col1View extends Component {
+
+  /**
+   * Handle select picture button click
+   */
+  handleSelectPicture() {
+    const pictureInput = $(this.pictureInput);
+    pictureInput.trigger('click');
+  }
+
+
+  /**
+   * Handle picture input change
+   */
+  handleInputChange() {
+    const tree = this.props.tree;
+    const pictureInput = this.pictureInput;
+    const file = pictureInput.files[0];
+
+    if (file) {
+      if (window.URL.createObjectURL) {
+        const imageLink = window.URL.createObjectURL(file);
+        tree.set(['person', 'picture'], imageLink);
+      }
+    }
+  }
+
+
+  render() {
     return (
       <div className="editperson-col-1">
         <div className="col-1-img">
           <img className="img-responsive img-thumbnail" alt="" src={this.props.person.picture}/>
-          <input name="picture"
-                 type="file" accept="image/*" className="hidden js-picture-input"/>
+          <input ref={(ref) => this.pictureInput = ref}
+                 onChange={this.handleInputChange.bind(this)}
+              name="picture" type="file" accept="image/*" className="hidden"/>
         </div>
         <div className="col-1-buttons">
-          <button
-              className="btn btn-success">Select</button>
-          <button className="btn btn-danger">Delete</button>
+          <a onClick={this.handleSelectPicture.bind(this)}
+              className="btn btn-success">Select</a>
+          <a className="btn btn-danger">Delete</a>
         </div>
       </div>
     );
   }
-});
+};
