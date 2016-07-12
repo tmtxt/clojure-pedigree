66968d7eef97bfc367682f06a8b909a594d3145c
Truong <me@truongtx.me>
2016-07-12 10:14:52 +0700

    toggle first level

--- dev.frontend/js/pages/tree_view/data.js
+++ dev.frontend/js/pages/tree_view/data.js
@@ -1,8 +1,31 @@
 const api = require('API');
 const apiTree = api.tree;
 
+
+function toggle(d) {
+  if (d.children) {
+    d._children = d.children;
+    d.children = null;
+  } else {
+    d.children = d._children;
+    d._children = null;
+  }
+}
+
+
+function toggleAll(d) {
+  if (d.children) {
+    d.children.forEach(toggleAll);
+    toggle(d)
+  }
+}
+
+
 exports.getData = async function(tree) {
   const pedigreeTree = await apiTree.getTreeData();
+  if (pedigreeTree.children) {
+    pedigreeTree.children.forEach(toggleAll);
+  }
 
   tree.set('pedigreeTree', pedigreeTree);
   tree.set('initializing', false);
