d2cd2a2712ee93460a1396d857dc6b1d1fc49ba0
Truong <me@truongtx.me>
2016-08-28 19:45:33 +0700

    auto detect project dir

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-ansible-playbook -i "localhost," -c local ansible/main.yml
+ansible-playbook -i "localhost," -c local --extra-vars "{\"project_dir\": \"$PWD\"}" ansible/main.yml
